dafc74966179978291c16805e0a458ac4c75a33a
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-08-14 15:57:05 +0700

    create reusable todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   return (
     <div className="app">
